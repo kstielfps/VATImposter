9daf44fbd2ccdc9e076ffa3e3306e99398896ee6
kstiel <gabriel_siqueira_araujo1996@hotmail.com>
2025-11-15 02:01:31 -0300

    Add missing imports (os, logging) to views.py

--- game/views.py
+++ game/views.py
@@ -6,6 +6,8 @@ from django.conf import settings
 from django.contrib.auth import get_user_model
 import json
 import traceback
+import os
+import logging
 from .models import Game, Player
 
 User = get_user_model()
